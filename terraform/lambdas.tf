2a5b66c5ed2439fdc093414a038d0e29d17da996
Brandon Fulljames <bfullj@gmail.com>
2023-08-26 23:52:49 +0900

    Add dynamodb policy

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -9,3 +9,29 @@ module "lambda_api" {
     EVERTRAS_LEADERBOARD_TABLE = aws_dynamodb_table.events.name
   }
 }
+
+resource "aws_iam_policy" "api_db_policy" {
+  name        = "${local.prefix}-api-db-policy"
+  description = "API access to write/query database"
+  policy      = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "dynamodb:Query",
+       "dynamodb:PutItem",
+       "dynamodb:GetItem"
+     ],
+     "Resource": "${aws_dynamodb_table.events.arn}",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_api_db_policy" {
+  role       = module.lambda_api.role_name
+  policy_arn = aws_iam_policy.api_db_policy.arn
+}
