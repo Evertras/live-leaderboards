8c2a021fccfbac920678be2787f74c1588f943d9
Brandon Fulljames <bfullj@gmail.com>
2023-08-26 22:33:25 +0900

    Table with scorecard info

--- site/src/components/Scorecard/index.tsx
+++ site/src/components/Scorecard/index.tsx
@@ -7,7 +7,31 @@ export interface ScorecardProps {
   round: Round;
 }
 
+// TODO: Figure out how to automate this better, it's based off API
+// spec but the generator doesn't properly make it...
+interface PlayerData {
+  name: string;
+  scores: { hole: number; score: number }[];
+}
+
 const Scorecard = ({ round }: ScorecardProps) => {
+  const playerData = round.players.map((p: PlayerData) => {
+    const holeScores = round.course.holes.map(() => null);
+
+    if (p.scores) {
+      for (const score of p.scores) {
+        holeScores[score.hole - 1] = score.score;
+      }
+    } else {
+      // Testing
+      holeScores[0] = p.name.length;
+    }
+
+    return {
+      name: p.name,
+      scores: holeScores,
+    };
+  });
   return (
     <React.Fragment>
       <div className={styles.Scorecard}>
@@ -33,9 +57,12 @@ const Scorecard = ({ round }: ScorecardProps) => {
             </tr>
           </thead>
           <tbody>
-            {round.players.map((p: any, i: number) => (
+            {playerData.map((p: any, i: number) => (
               <tr key={i}>
                 <td>{p.name}</td>
+                {p.scores.map((s: number | null, i: number) => {
+                  return <td key={i}>{s}</td>;
+                })}
               </tr>
             ))}
           </tbody>
