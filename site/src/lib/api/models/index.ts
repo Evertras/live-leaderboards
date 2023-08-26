33a7df763af0fff69f7b6729321eaeb7429461ca
Brandon Fulljames <bfullj@gmail.com>
2023-08-26 17:30:56 +0900

    Generate typescript client for API

---
+++ site/src/lib/api/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./Course";
+export * from "./CreatedRound";
+export * from "./Hole";
+export * from "./HoleScore";
+export * from "./PlayerData";
+export * from "./Round";
+export * from "./RoundRequest";
