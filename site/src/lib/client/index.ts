31f056f6d93b686268623a2301f7e29fee255051
Brandon Fulljames <bfullj@gmail.com>
2023-08-26 23:20:40 +0900

    Configurable API endpoint

--- site/src/lib/client/index.ts
+++ site/src/lib/client/index.ts
@@ -1,9 +1,8 @@
 import { Configuration, Round, RoundApi } from "../api";
 
 const configuration = new Configuration({
-  // TODO: Config this on build or use
-  // basePath: window.location.origin
-  basePath: "http://localhost:8037",
+  basePath:
+    process.env.REACT_APP_EVERTRAS_API_BASE_PATH ?? "http://localhost:8037",
 });
 
 const roundAPI = new RoundApi(configuration);
