08f338490fa058ac68f2b0eef2d01d60809f25f4
Brandon Fulljames <bfullj@gmail.com>
2023-08-27 20:18:33 +0900

    Still broken but improving with new gen

--- site/src/lib/api/models/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from "./Course";
-export * from "./CreatedRound";
-export * from "./Hole";
-export * from "./HoleScore";
-export * from "./PlayerData";
-export * from "./Round";
-export * from "./RoundRequest";
