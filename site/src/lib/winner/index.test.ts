1f03b241d659f48f83dc127a6ca6ea150ba59f62
Brandon Fulljames <bfullj@gmail.com>
2023-09-03 12:19:38 +0900

    More matchplay score calculation tests (#21)
    * More matchplay score calculation tests
    
    * One more test

--- site/src/lib/winner/index.test.ts
+++ site/src/lib/winner/index.test.ts
@@ -1,20 +1,96 @@
-import { calcCurrentMatchplayWinner } from ".";
+import { calcCurrentMatchplayWinner, MatchplayResult } from ".";
 import { Round } from "../client";
 
-describe("matchplay result calculations", () => {
-  test("no one is winning an empty round", () => {
-    const round = genRound();
-    const result = calcCurrentMatchplayWinner(round);
+describe("matchplay 1v1 result calculations", () => {
+  const testCases: {
+    name: string;
+    scoresA: number[];
+    scoresB: number[];
+    expectedResult: MatchplayResult;
+  }[] = [
+    {
+      name: "empty round",
+      scoresA: [],
+      scoresB: [],
+      expectedResult: {
+        totalHolesWonByPlayerIndex: [0, 0],
+        currentWinnerPlayerIndex: null,
+        upBy: 0,
+      },
+    },
+    {
+      name: "tied first",
+      scoresA: [3],
+      scoresB: [3],
+      expectedResult: {
+        totalHolesWonByPlayerIndex: [0, 0],
+        currentWinnerPlayerIndex: null,
+        upBy: 0,
+      },
+    },
+    {
+      name: "player A won first",
+      scoresA: [3],
+      scoresB: [4],
+      expectedResult: {
+        totalHolesWonByPlayerIndex: [1, 0],
+        currentWinnerPlayerIndex: 0,
+        upBy: 1,
+      },
+    },
+    {
+      name: "player B won first",
+      scoresA: [4],
+      scoresB: [3],
+      expectedResult: {
+        totalHolesWonByPlayerIndex: [0, 1],
+        currentWinnerPlayerIndex: 1,
+        upBy: 1,
+      },
+    },
+    {
+      name: "each player wins one",
+      scoresA: [4, 4],
+      scoresB: [3, 5],
+      expectedResult: {
+        totalHolesWonByPlayerIndex: [1, 1],
+        currentWinnerPlayerIndex: null,
+        upBy: 0,
+      },
+    },
+    {
+      name: "all holes tied",
+      scoresA: [8, 2],
+      scoresB: [8, 2],
+      expectedResult: {
+        totalHolesWonByPlayerIndex: [0, 0],
+        currentWinnerPlayerIndex: null,
+        upBy: 0,
+      },
+    },
+    {
+      name: "all holes won by player B",
+      scoresA: [9, 3],
+      scoresB: [8, 2],
+      expectedResult: {
+        totalHolesWonByPlayerIndex: [0, 2],
+        currentWinnerPlayerIndex: 1,
+        upBy: 2,
+      },
+    },
+  ];
 
-    expect(result.upBy).toEqual(0);
-    expect(result.currentWinnerPlayerIndex).toBeNull();
-    expect(result.totalHolesWonByPlayerIndex).toEqual(
-      new Array(round.players.length).fill(0),
-    );
+  testCases.forEach((testCase) => {
+    test(testCase.name, () => {
+      const round = gen1v1Round(testCase.scoresA, testCase.scoresB);
+      const result = calcCurrentMatchplayWinner(round);
+
+      expect(result).toEqual(testCase.expectedResult);
+    });
   });
 });
 
-function genRound(): Round {
+function gen1v1Round(scoresA: number[], scoresB: number[]): Round {
   return {
     id: "test-round",
     title: "Test Round",
@@ -36,11 +112,11 @@ function genRound(): Round {
     players: [
       {
         name: "Player A",
-        scores: [],
+        scores: scoresA.map((n, i) => ({ hole: i + 1, score: n })),
       },
       {
         name: "Player B",
-        scores: [],
+        scores: scoresB.map((n, i) => ({ hole: i + 1, score: n })),
       },
     ],
   };
