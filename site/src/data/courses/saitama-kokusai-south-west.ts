1e238d6a5eda873e7f15a7d66fc0784bb6dfbbee
Brandon Fulljames <bfullj@gmail.com>
2023-11-23 19:27:43 +0900

    Add Saitama Kokusai and make it the default create (#31)

---
+++ site/src/data/courses/saitama-kokusai-south-west.ts
@@ -0,0 +1,118 @@
+import { Course } from "../../lib/client";
+
+const data: Course = {
+  name: "Saikama Kokusai South West",
+  tees: "White",
+  holes: [
+    {
+      hole: 1,
+      par: 4,
+      distanceYards: 353,
+      strokeIndex: 5,
+    },
+    {
+      hole: 2,
+      par: 3,
+      distanceYards: 142,
+      strokeIndex: 17,
+    },
+    {
+      hole: 3,
+      par: 4,
+      distanceYards: 315,
+      strokeIndex: 11,
+    },
+    {
+      hole: 4,
+      par: 5,
+      distanceYards: 575,
+      strokeIndex: 1,
+    },
+    {
+      hole: 5,
+      par: 4,
+      distanceYards: 281,
+      strokeIndex: 13,
+    },
+    {
+      hole: 6,
+      par: 4,
+      distanceYards: 348,
+      strokeIndex: 9,
+    },
+    {
+      hole: 7,
+      par: 3,
+      distanceYards: 365,
+      strokeIndex: 3,
+    },
+    {
+      hole: 8,
+      par: 5,
+      distanceYards: 444,
+      strokeIndex: 7,
+    },
+    {
+      hole: 9,
+      par: 3,
+      distanceYards: 150,
+      strokeIndex: 15,
+    },
+    {
+      hole: 10,
+      par: 3,
+      distanceYards: 117,
+      strokeIndex: 18,
+    },
+    {
+      hole: 11,
+      par: 3,
+      distanceYards: 89,
+      strokeIndex: 17,
+    },
+    {
+      hole: 12,
+      par: 5,
+      distanceYards: 476,
+      strokeIndex: 6,
+    },
+    {
+      hole: 13,
+      par: 4,
+      distanceYards: 386,
+      strokeIndex: 4,
+    },
+    {
+      hole: 14,
+      par: 4,
+      distanceYards: 307,
+      strokeIndex: 12,
+    },
+    {
+      hole: 15,
+      par: 4,
+      distanceYards: 279,
+      strokeIndex: 14,
+    },
+    {
+      hole: 16,
+      par: 3,
+      distanceYards: 151,
+      strokeIndex: 16,
+    },
+    {
+      hole: 17,
+      par: 4,
+      distanceYards: 314,
+      strokeIndex: 8,
+    },
+    {
+      hole: 18,
+      par: 5,
+      distanceYards: 467,
+      strokeIndex: 2,
+    },
+  ],
+};
+
+export default data;
